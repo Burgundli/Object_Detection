c761696ae25b7a1885c553efd823fb2119bb8dfa
Daniel Gemer <gemer.daniel@gmail.com>
2020-09-13 20:34:35 +0200

    Default code for opening kinect sensor

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,17 +14,131 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Kinect;
+using Emgu.CV;
+using Emgu.CV.Structure;
 
 namespace Object_Detection
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+
+        private const int  DepthToByte = 8000 / 256;        // - constant for conversion from depth distance in meters to 256 byte format 
+        private KinectSensor kinectSensor = null;           // - variable reporesenting the active sensor 
+        private DepthFrameReader depthFrameReader = null;   // - variable representing the depth frame reader 
+        private FrameDescription frameDescription = null;   // - description of the data contained in the depth frame 
+        private WriteableBitmap depthbitmap = null;         // - bitmap for displaying image 
+        private byte[] depthpixels = null;                  // - intermediate storage for frame datta conveted to color pointer 
+        private string KinectStatus = null;                 // - current status of the kinect sensor 
+
         public MainWindow()
         {
+            // Set up of  the Sensor and reader 
+            kinectSensor = KinectSensor.GetDefault();
+            depthFrameReader = kinectSensor.DepthFrameSource.OpenReader();
+            // Create and event handler for the arrived frames 
+            depthFrameReader.FrameArrived += DepthFrameReader_FrameArrived;
+            frameDescription = kinectSensor.DepthFrameSource.FrameDescription;
+            // Allocate memory for the arrived and converted frames then store them to bitmap  
+            depthpixels = new byte[frameDescription.Width * frameDescription.Height];
+            depthbitmap = new WriteableBitmap(frameDescription.Width, frameDescription.Height, 96.0, 96.0, PixelFormats.Gray8, null);
+            kinectSensor.IsAvailableChanged += Sensor_IsAvaibleChanged;
+            kinectSensor.Open();
+            KinectStatus = kinectSensor.IsAvailable ? "Running" : "Turned off";
+          
+
+           
             InitializeComponent();
+        }
+        public ImageSource ImageSource
+        {
+            get
+            {
+                return this.depthbitmap; 
+            }
+        }
+        public string StatusText
+        {
+            get
+            {
+                return KinectStatus; 
+            }
+           
+        }
+
+
+        private void Sensor_IsAvaibleChanged(object sender, IsAvailableChangedEventArgs e)
+        {
+
+            KinectStatus = kinectSensor.IsAvailable ? "Running" : "Turned off";
+
+           
+        }
+
+        private void DepthFrameReader_FrameArrived(object sender, DepthFrameArrivedEventArgs e)
+        {
+            bool IsFrameProcessed = false;
+            using (DepthFrame depthFrame = e.FrameReference.AcquireFrame())
+            {
+                if (depthFrame != null)
+                {
+                    using (KinectBuffer kinectBuffer = depthFrame.LockImageBuffer())
+                    {
+                        if (((frameDescription.Width*frameDescription.Height) == (kinectBuffer.Size / frameDescription.BytesPerPixel)) && (frameDescription.Width == depthbitmap.PixelWidth) && (frameDescription.Height == depthbitmap.PixelHeight))
+                        {
+                            ushort maxDepthValue = ushort.MaxValue;
+                            ProcessDepthFrameData(kinectBuffer.UnderlyingBuffer, kinectBuffer.Size, depthFrame.DepthMinReliableDistance, maxDepthValue);
+                            IsFrameProcessed = true;
+                        }
+                    }
+
+
+                }
+            }
+            if (IsFrameProcessed)
+            {
+                RenderPixels(); 
+            }
+
+               
+        }
+        private unsafe void ProcessDepthFrameData (IntPtr depthFrameData, uint depthFrameDataSize, ushort minDepth, ushort maxDepth)
+        {
+            ushort*  framedata = (ushort*)depthFrameData;
+
+            for (int i = 0; i < (int)(depthFrameDataSize / frameDescription.BytesPerPixel); ++i)
+            {
+                ushort depth = framedata[i];
+                depthpixels[i] = (byte)(depth >= minDepth && depth <= maxDepth ? (depth / DepthToByte) : 0);
+
+            }
+
+
+        }
+        private void RenderPixels()
+        {
+            depthbitmap.WritePixels( new Int32Rect(0, 0, this.depthbitmap.PixelWidth, this.depthbitmap.PixelHeight), this.depthpixels, this.depthbitmap.PixelWidth,0);
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void Window_Closing(object sender, CancelEventArgs e)
+        {
+            if (depthFrameReader != null)
+            {
+                depthFrameReader.Dispose();
+                depthFrameReader = null; 
+            }
+            if (kinectSensor != null)
+            {
+                kinectSensor.Close();
+                kinectSensor = null; 
+            }
+           
         }
     }
 }
